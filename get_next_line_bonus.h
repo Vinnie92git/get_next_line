8a343bd9db8934c979102164f765d39fc6e7c3eb
Vinnie92git <112777433+Vinnie92git@users.noreply.github.com>
2023-04-13 12:53:27 +0200

    Add files via upload

---
+++ get_next_line_bonus.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vipalaci <vipalaci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/24 15:55:02 by vipalaci          #+#    #+#             */
+/*   Updated: 2023/02/24 15:55:14 by vipalaci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_read_and_save(int fd, char *saved);
+char	*ft_find_line(char *saved);
+char	*ft_next_line(char *saved);
+size_t	ft_strlen(char *str);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_join(char *dest, char *s1, char *s2);
+
+#endif
